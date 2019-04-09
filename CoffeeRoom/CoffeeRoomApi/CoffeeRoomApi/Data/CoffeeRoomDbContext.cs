7f8360b6c112b39d35ec37d7d42ad172ea82aed0
knromaric <nzekeng@gmail.com>
2019-04-09 18:41:35 +0300

    Added DbContext class and connection string

---
+++ CoffeeRoom/CoffeeRoomApi/CoffeeRoomApi/Data/CoffeeRoomDbContext.cs
@@ -0,0 +1,16 @@
+﻿using CoffeeRoomApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace CoffeeRoomApi.Data
+{
+    public class CoffeeRoomDbContext : DbContext
+    {
+        public DbSet<Menu> Menus { get; set; }
+        public DbSet<Reservation> Reservations { get; set; }
+
+    }
+}
