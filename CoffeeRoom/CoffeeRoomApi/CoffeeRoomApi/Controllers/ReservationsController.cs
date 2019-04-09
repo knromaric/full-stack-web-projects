385009eae286b3fe91e07737234ac47cc7dae2c9
knromaric <nzekeng@gmail.com>
2019-04-09 19:34:46 +0300

    Added Reservation Controller

---
+++ CoffeeRoom/CoffeeRoomApi/CoffeeRoomApi/Controllers/ReservationsController.cs
@@ -0,0 +1,26 @@
+﻿using CoffeeRoomApi.Data;
+using CoffeeRoomApi.Models;
+using System.Net;
+using System.Web.Http;
+
+namespace CoffeeRoomApi.Controllers
+{
+    public class ReservationsController : ApiController
+    {
+        private CoffeeRoomDbContext _dbContext = new CoffeeRoomDbContext();
+        
+        [HttpPost]
+        public IHttpActionResult CreateReservation([FromBody]Reservation reservation)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _dbContext.Reservations.Add(reservation);
+            _dbContext.SaveChanges();
+
+            return StatusCode(HttpStatusCode.Created);
+        }
+    }
+}
--- CoffeeRoom/CoffeeRoomApi/CoffeeRoomApi/Models/SubMenu.cs
+++ CoffeeRoom/CoffeeRoomApi/CoffeeRoomApi/Models/SubMenu.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace CoffeeRoomApi.Models
+﻿namespace CoffeeRoomApi.Models
 {
     public class SubMenu
     {
